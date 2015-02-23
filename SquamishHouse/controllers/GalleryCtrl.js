db224076db5887ab972a111f433188749dc08b5a
Regan <reganlperkins@gmail.com>
2015-02-22 21:22:12 -0800

    starting new repo from crash pad

---
+++ SquamishHouse/controllers/GalleryCtrl.js
@@ -0,0 +1,18 @@
+angular.module('squamishApp')
+  .controller('GalleryCtrl', function($scope){
+
+    $scope.bricks = [
+      'images/living-fireplace.jpg',
+      'images/kitchen.jpg',
+      'images/living-room.jpg',
+      'images/livingroom.jpg',
+      'images/mstr-bathroom.jpg',
+      'images/mstr-bed.jpg',
+      'images/mstr-bedroom.jpg',
+      'images/mstr-shower.jpg',
+      'images/mstr-tub.jpg',
+      'images/sauna.jpg'
+    ];
+    
+  });
+

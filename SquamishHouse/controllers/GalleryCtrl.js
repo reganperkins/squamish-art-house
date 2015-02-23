db9543e777e37aa7fc6aaf0d3a4364392808a5fb
Regan <reganlperkins@gmail.com>
2015-02-23 00:12:59 -0800

    add font awesome and implemented gallery

--- SquamishHouse/controllers/GalleryCtrl.js
+++ SquamishHouse/controllers/GalleryCtrl.js
@@ -2,16 +2,16 @@ angular.module('squamishApp')
   .controller('GalleryCtrl', function($scope){
 
     $scope.bricks = [
-      'images/living-fireplace.jpg',
-      'images/kitchen.jpg',
-      'images/living-room.jpg',
-      'images/livingroom.jpg',
-      'images/mstr-bathroom.jpg',
-      'images/mstr-bed.jpg',
-      'images/mstr-bedroom.jpg',
-      'images/mstr-shower.jpg',
-      'images/mstr-tub.jpg',
-      'images/sauna.jpg'
+      {src: 'images/living-fireplace.jpg'},
+      {src: 'images/kitchen.jpg'},
+      {src: 'images/living-room.jpg'},
+      {src: 'images/livingroom.jpg'},
+      {src: 'images/mstr-bathroom.jpg'},
+      {src: 'images/mstr-bed.jpg'},
+      {src: 'images/mstr-bedroom.jpg'},
+      {src: 'images/mstr-shower.jpg'},
+      {src: 'images/mstr-tub.jpg'},
+      {src: 'images/sauna.jpg'}
     ];
     
   });

63b1d3f3e8a64a966e6561bb0cb38a7f0552e5af
Regan <reganlperkins@gmail.com>
2015-03-05 15:27:47 -0800

    move map function into controller + add parlax - bugs still present

--- app/MainPage/controllers/MainCtrl.js
+++ app/MainPage/controllers/MainCtrl.js
@@ -1,4 +1,23 @@
 angular.module('squawsomeApp')
   .controller('MainCtrl', function(){
     
+    angular.element(document).ready(function initialize() {
+      var myLatlng = new google.maps.LatLng(49.697344, -123.135071);
+      var mapCanvas = document.getElementById('map-canvas');
+      var mapOptions = {
+        center: new google.maps.LatLng(49.7017, -123.1588),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+      var map = new google.maps.Map(mapCanvas, mapOptions)
+
+      var marker = new google.maps.Marker({
+          position: myLatlng,
+          map: map,
+          title: 'Art House'
+      });
+    });
+
+    // google.maps.event.addDomListener(window, 'load', initialize);
+
   });

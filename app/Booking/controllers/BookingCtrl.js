abe6b18a234f74b2e2ef3c57da63fd8d495d6d7c
Regan <reganlperkins@gmail.com>
2015-03-09 17:59:44 -0700

    add booking to ctrl

--- app/Booking/controllers/BookingCtrl.js
+++ app/Booking/controllers/BookingCtrl.js
@@ -1,4 +1,22 @@
 angular.module('squawsomeApp')
   .controller('BookingCtrl', function(){
-    
+    this.booking = {
+      // checkin,
+      // checkout,
+      // guests,
+      // nights,
+      cost: function(){
+        if (this.nights < 7) {
+          cost = 145;
+        } else if (this.nights >= 7 && this.nights <= 29){
+          cost = 135;
+        } else if (this.nights >= 29){
+          cost = 120;
+        }
+      },
+      pretotal: this.nights * this.cost,
+      tax: 0,
+      total: this.pretotal + this.tax
+
+    };
   });
